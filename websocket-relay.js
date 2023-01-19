90927c50cc7ebddfe9714a8da494370bda6b8fc9
anirudh <67142496+anirudh1601@users.noreply.github.com>
2023-01-19 05:09:52 -0600

    Update websocket-relay.js

--- websocket-relay.js
+++ websocket-relay.js
@@ -21,6 +21,7 @@ var STREAM_SECRET = process.argv[2],
 
 
 var streamServer = http.createServer(function(request, response) {
+	response.connection.setTimeout(0);
         Console.log("connected")
 	console.log(
 		'Stream Connected: ' +
